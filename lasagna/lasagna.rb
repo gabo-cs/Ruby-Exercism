abc0bd5a37e4d5471b032478e8054e8b886e69b3
Gabo <gabrielomar2809@gmail.com>
2021-09-16 21:38:17 -0500

    Lasagna

---
+++ lasagna/lasagna.rb
@@ -0,0 +1,19 @@
+# In this exercise you're going to write some code to help you cook a brilliant
+# lasagna from your favorite cooking book.
+
+# You have four tasks, all related to the time spent cooking the lasagna.
+class Lasagna
+  EXPECTED_MINUTES_IN_OVEN = 40
+
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+  end
+
+  def preparation_time_in_minutes(layers)
+    layers * 2
+  end
+
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+  end
+end
